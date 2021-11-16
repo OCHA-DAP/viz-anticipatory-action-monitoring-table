9fe63d1cc0bb5a603a5debb8b59b7dd6db851599
baripembo <erikawei@gmail.com>
2021-11-16 18:36:42 -0500

    init

--- src/js/viz.js
+++ src/js/viz.js
@@ -1,26 +1,81 @@
 $( document ).ready(function() {
-  const DATA_URL = 'data/';
+  const DATA_URL = 'https://proxy.hxlstandard.org/data.objects.json?dest=data_edit&strip-headers=on&force=on&header-row=1&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vSuwMFCg_aLAghw4CzGeL5xpGimXi4k4dFmqpvlIAt4wzZYU8GnmRANLT6dHOZwe0FpFmQ4r_Bd7iyy%2Fpub%3Fgid%3D0%26single%3Dtrue%26output%3Dcsv';
   let isMobile = $(window).width()<600? true : false;
-  let dataUrls = ['geodata_locations.geojson'];
 
   function getData() {
-    dataUrls.forEach(function (url, index) {
-      loadData(url, function (responseText) {
-        parseData(JSON.parse(responseText), index);
-      })
-    })
+
+    d3.json(DATA_URL).then(function(data) {
+      const columns = ['Country', 'Shock', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      
+      //console.log(data)
+      let items = [];
+      for (const item of data) {
+        let obj = {};
+        let dateRange = item['#date+range'].split(',');
+
+        for (const column of columns) {
+          if (column=='Country') {
+            obj[column] = item['#country+name'];
+          }
+          else if (column=='Shock') {
+            obj[column] = item['#event+name'];
+          }
+          else {
+            let active = false;
+            for (const date of dateRange) {
+              if (date==column) active = true;
+            }
+            obj[column] = active;
+          }
+        }
+        items.push(obj);
+      }
+
+      let table = d3.select('.table-container').append('table');
+      let headers = table.append('thead').append('tr')
+         .selectAll('th')
+         .data(columns).enter()
+         .append('th')
+         .attr('class', function(d) {
+            let thClass = (d!=='Country' && d!=='Shock') ? 'monthHeader' : '';
+            return thClass;
+         })
+         .text(function (d) {
+            return d;
+          });
+      
+      let rows = table.append('tbody').selectAll('tr')
+        .data(items).enter()
+        .append('tr');
+      
+      rows.selectAll('td')
+        .data(function (d) {
+          return columns.map(function (col) {
+            //console.log(k)
+            return { 'name': col, 'value': d[col] };
+          });
+        }).enter()
+        .append('td')
+        .attr('data-th', function (d) {
+          return d.name;
+        })
+        .attr('class', function (d) {
+          let hasClass = '';
+          if (d.name!=='Country' && d.name!=='Shock') {
+            if (d.value) hasClass = 'bg';
+          }
+          return hasClass;
+        })
+        .text(function (d) {
+          if (d.name=='Country' || d.name=='Shock') {
+            return d.value;
+          }
+        });
+    });
   }
 
-  function loadData(dataPath, done) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () { return done(this.responseText) }
-    xhr.open('GET', DATA_URL+dataPath, true);
-    xhr.send();
-  }
+  function createTable() {
 
-  function parseData(geoData, index) {
-    //do something with the data
-    console.log(geoData, index)
   }
 
   function initTracking() {
