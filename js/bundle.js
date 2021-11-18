3ed67ec63a63203e4a7a3f97a8c2974e60f2deac
baripembo <erikawei@gmail.com>
2021-11-18 15:07:35 -0500

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -35,7 +35,9 @@ $( document ).ready(function() {
     d3.json(DATA_URL).then(function(data) {
       const columns = ['Country', 'Shock', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
       
-      //console.log(data)
+      data = data.sort((a, b) => (a['#country+name'] > b['#country+name']) ? 1 : -1)
+
+      console.log(data)
       let items = [];
       for (const item of data) {
         let obj = {};
@@ -79,7 +81,6 @@ $( document ).ready(function() {
       rows.selectAll('td')
         .data(function (d) {
           return columns.map(function (col) {
-            //console.log(k)
             return { 'name': col, 'value': d[col] };
           });
         }).enter()
@@ -100,10 +101,6 @@ $( document ).ready(function() {
           }
         });
     });
-  }
-
-  function createTable() {
-
   }
 
   function initTracking() {
